a98f8589c49db9aede6f9e467e953af85051e2b0
johnstarberry <johnpaul@starberry.tv>
2023-01-19 14:24:09 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,12 @@ import Login from './components/login-page';
 import Otp from './components/otp';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Test from './components/test';
-import Questions from './components/questions';
-
+import { Easy } from './components/easy';
+import { Modurate } from './components/modurate';
+import { Advance } from './components/advance';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import { Header } from './components/header';
 function App() {
   return (
    <>
@@ -12,7 +16,9 @@ function App() {
    <Route path="/" element = {<Login/>}/>
     <Route path="/otp" element = {<Otp/>}/>
     <Route path='/test' element={<Test/>}/>
-    <Route path='/questions' element={<Questions/>}/>
+    <Route path='/questions' element={<Easy/>}/>
+    <Route path='/modurate' element={<Modurate/>}/>
+    <Route path='/advance' element={<Advance/>}/>
    </Routes>
    </BrowserRouter>
    </>
